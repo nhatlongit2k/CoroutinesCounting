7de911a880d6d069349f47e57a84430b31f94d11
LongTienSinh <nhatlongit2k@gmail.com>
2021-10-05 02:16:13 +0700

    add function

--- app/src/main/java/com/example/coroutinescounting/MainActivity.kt
+++ app/src/main/java/com/example/coroutinescounting/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.coroutinescounting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.coroutines.*
+import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +17,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvNumber: TextView
     lateinit var btPlus: Button
     lateinit var btMinus: Button
+    lateinit var linearLayout: LinearLayout
+    var isPress: Boolean = false
     var count: Int = 0
     var job: Job? =null
+    var yPress: Int =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,53 +31,130 @@ class MainActivity : AppCompatActivity() {
 
 
         runBlocking {
-            btPlus.setOnClickListener {
-                count++
-                tvNumber.setText(count.toString())
+
+            btPlus.setOnTouchListener { view, motionEvent ->
                 job?.cancel()
-                job = GlobalScope.launch(Dispatchers.Main) {
-                    delay(1000)
-                    while (count!=0){
-                        if(count>0){
-                            delay(500)
-                            count--
-                            tvNumber.setText(count.toString())
+                when(motionEvent.action){
+                    MotionEvent.ACTION_DOWN -> {
+                        isPress = true
+                        Log.d("PRESS", "onCreate: PRESSS")
+                        GlobalScope.launch(Dispatchers.Main) {
+                            while (isPress == true){
+                                count++
+                                Log.d("PRESS", "onCreate: $count")
+                                tvNumber.setText(count.toString())
+                                delay(200)
+                            }
                         }
-                        else{
-                            delay(500)
-                            count++
-                            tvNumber.setText(count.toString())
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        isPress=false
+                        Log.d("PRESS", "onCreate: PRESSS-UP")
+                        job = GlobalScope.launch(Dispatchers.Main) {
+                            delay(1000)
+                            while (count!=0){
+                                if(count>0){
+                                    delay(300)
+                                    count--
+                                    Log.d("PRESS", "onCreate: Count --")
+                                    tvNumber.setText(count.toString())
+                                }
+                                else{
+                                    delay(300)
+                                    count++
+                                    tvNumber.setText(count.toString())
+                                }
+                            }
                         }
                     }
                 }
+                true
             }
-            btMinus.setOnClickListener {
-                count--
-                tvNumber.setText(count.toString())
+
+            btMinus.setOnTouchListener { view, motionEvent ->
                 job?.cancel()
-                job = GlobalScope.launch(Dispatchers.Main) {
-                    delay(1000)
-                    while (count!=0){
-                        if(count>0){
-                            delay(500)
-                            count--
-                            tvNumber.setText(count.toString())
+                when(motionEvent.action){
+                    MotionEvent.ACTION_DOWN -> {
+                        isPress = true
+                        Log.d("PRESS", "onCreate: PRESSS")
+                        GlobalScope.launch(Dispatchers.Main) {
+                            while (isPress == true){
+                                count--
+                                Log.d("PRESS", "onCreate: $count")
+                                tvNumber.setText(count.toString())
+                                delay(200)
+                            }
                         }
-                        else{
-                            delay(500)
-                            count++
-                            tvNumber.setText(count.toString())
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        isPress=false
+                        Log.d("PRESS", "onCreate: PRESSS-UP")
+                        job = GlobalScope.launch(Dispatchers.Main) {
+                            delay(1000)
+                            while (count!=0){
+                                if(count>0){
+                                    delay(300)
+                                    count--
+                                    Log.d("PRESS", "onCreate: Count --")
+                                    tvNumber.setText(count.toString())
+                                }
+                                else{
+                                    delay(300)
+                                    count++
+                                    tvNumber.setText(count.toString())
+                                }
+                            }
                         }
                     }
                 }
+                true
             }
         }
 
+        linearLayout.setOnTouchListener { view, motionEvent ->
+            job?.cancel()
+            when(motionEvent.action){
+                MotionEvent.ACTION_DOWN -> {
+
+                    yPress = motionEvent.y.toInt()
+                    isPress = true
+                    Log.d("PRESS", "onCreate: PRESSS: $yPress")
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    var y = (motionEvent.y - yPress).toInt()
+                    count = count - y
+                    tvNumber.setText(count.toString())
+                    yPress = motionEvent.y.toInt()
+                }
+                MotionEvent.ACTION_UP -> {
+                    Log.d("PRESS", "onCreate: PRESSS-UP")
+                    job = GlobalScope.launch(Dispatchers.Main) {
+                        delay(1000)
+                        while (count!=0){
+                            if(count>0){
+                                delay(300)
+                                count--
+                                Log.d("PRESS", "onCreate: Count --")
+                                tvNumber.setText(count.toString())
+                            }
+                            else{
+                                delay(300)
+                                count++
+                                tvNumber.setText(count.toString())
+                            }
+                        }
+                    }
+                }
+            }
+
+            true
+        }
     }
 
     private fun initView() {
         tvNumber = findViewById(R.id.tvnumber)
         btMinus = findViewById(R.id.btminus)
         btPlus = findViewById(R.id.btphus)
+        linearLayout = findViewById(R.id.linearLayoutnumber)
     }
 }
